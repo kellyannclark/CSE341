fcd409cc1aa299360b2e4400e1b5d22fe8d32e8f
kellyannclark <thefaithseed@gmail.com>
2023-09-15 16:07:15 -0600

    changes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const routes = require("express").Router();
+const lesson1Controller = require("./controllers/lesson1")
+
+ 
+routes.get('/', (req, res) => {
+  res.send("Hello");})
+
+routes.get('/kelly', lesson1Controller.helloKelly);
+
+module.exports = routes;
